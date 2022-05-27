1bf7a84ed23be634b2ff9d1b6f3bc9168dd6a30e
Chandan <dmeher082@gmail.com>
2022-05-27 10:33:07 +0530

    commit

---
+++ All post/style/readme_button.js
@@ -0,0 +1,19 @@
+// readmore-btn 
+
+        function myFunction() {
+            var dots = document.getElementById("dots");
+            var moreText = document.getElementById("more");
+            var btnText = document.getElementById("readBtn");
+
+            if (dots.style.display === "none") {
+                dots.style.display = "inline";
+                btnText.innerHTML = "Read more";
+                moreText.style.display = "none";
+            } else {
+                dots.style.display = "none";
+                btnText.innerHTML = "Read less";
+                moreText.style.display = "inline";
+            }
+        }
+
+// readmore-btn-end
